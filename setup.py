cf060835c1c9c74b10e4e8b51daa09a1617b738d
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2020-09-10 09:32:48 -0500

    Update setup.py with new name

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='pyfooty',
         version='0.0',
         description='Parse the fbref website with python',
-        url='https://github.com/rmatsum836/pyfbref',
+        url='https://github.com/rmatsum836/pyfooty',
         author='Ray Matsumoto',
         author_email='raymatsum@gmail.com',
         license='MIT',
-        packages=['pyfbref'],
+        packages=['pyfooty'],
         zip_safe=False)
