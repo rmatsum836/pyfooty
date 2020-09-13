91a8f748c0942520b21a92456a6b9802b649dec9
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2020-09-13 14:54:21 -0500

    Add club object and move some functions to separate utils module

---
+++ pyfooty/src/utils.py
@@ -0,0 +1,28 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+def get_soup(fbref_url, name=None):
+    """ Get soup of FBRef URL
+
+    Parameters
+    ----------
+    fbref_url : str
+        Url of FBRef. Can either be the search url or an exact match url
+    name : str
+        Name to search for, must be used with the search url.
+    """
+    if name:
+        fbref_url += "=" + name
+    response = requests.get(fbref_url)
+    comm = re.compile("<!--|-->")
+    soup = BeautifulSoup(comm.sub("", response.text), "lxml")
+
+    return soup
+
+def get_available_tables(url, name=None):
+    """ Get available tables for specific player"""
+    soup = get_soup(url, name)
+    all_divs = soup.findAll("div", {"class": "table_wrapper"})
+    div = tuple([x.find("span")["data-label"] for x in all_divs])
+    return div
